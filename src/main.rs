5ae553bfb21aeff70a554d6277c1180618dee1f0
Shuntaro Nishizawa <me@s2n.tech>
2023-07-22 18:16:36 +0900

    chore: Remove network

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
+#[warn(clippy::all, clippy::pedantic)]
+#[allow(clippy::cast_lossless)]
+pub mod block;
+pub mod field;
+pub mod input;
+pub mod mino;
+pub mod movement;
+pub mod position;
+pub mod random;
+pub mod timer;
+
 use bevy::{
     diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
     log::LogPlugin,
     prelude::*,
     render::camera::ScalingMode,
 };
-use bevy_renet::{
-    renet::{DefaultChannel, RenetClient},
-    transport::NetcodeClientPlugin,
-    RenetClientPlugin,
-};
-use tetris::{
-    block::{transform_system, BLOCK_SIZE},
-    field::Field,
-    input::{keyboard_input_system, KeyboardRepeatTimer},
-    mino::event::{handle_place_mino, handle_spawn_mino, PlaceMinoEvent, SpawnMinoEvent},
-    movement::{handle_move_event, MoveEvent},
-    network::{renet_client, LocalPlayerId, ServerMessage},
-    timer::timer_system,
-};
+use block::{transform_system, BLOCK_SIZE};
+use field::Field;
+use input::{keyboard_input_system, KeyboardRepeatTimer};
+use mino::event::{handle_place_mino, handle_spawn_mino, PlaceMinoEvent, SpawnMinoEvent};
+use movement::{handle_move_event, MoveEvent};
+use timer::timer_system;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default, States)]
-pub enum GameState {
+pub enum AppState {
     #[default]
     MatchMaking,
     Playing,
@@ -30,8 +33,6 @@ pub enum GameState {
 struct FpsText;
 
 fn main() {
-    let (client, transport, local_player_id) = renet_client();
-
     App::new()
         .add_plugins(
             DefaultPlugins
@@ -48,23 +49,14 @@ fn main() {
                 }),
         )
         .add_plugins(FrameTimeDiagnosticsPlugin)
-        .add_plugins(RenetClientPlugin)
-        .add_plugins(NetcodeClientPlugin)
-        .add_state::<GameState>()
+        .add_state::<AppState>()
         .add_event::<SpawnMinoEvent>()
         .add_event::<PlaceMinoEvent>()
         .add_event::<MoveEvent>()
         .insert_resource(KeyboardRepeatTimer::default())
-        .insert_resource(client)
-        .insert_resource(transport)
-        .insert_resource(local_player_id)
         .add_systems(Startup, setup)
         .add_systems(Update, fps_system)
-        .add_systems(
-            Update,
-            (wait_for_players).run_if(in_state(GameState::MatchMaking)),
-        )
-        .add_systems(OnEnter(GameState::Playing), (setup_game,))
+        .add_systems(OnEnter(AppState::Playing), (setup_game,))
         .add_systems(
             Update,
             (
@@ -74,13 +66,13 @@ fn main() {
                 handle_spawn_mino,
                 handle_place_mino,
             )
-                .run_if(in_state(GameState::Playing)),
+                .run_if(in_state(AppState::Playing)),
         )
         .add_systems(PostUpdate, transform_system)
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, mut app_state: ResMut<NextState<AppState>>) {
     let mut camera_bundle = Camera2dBundle::default();
     camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(1000.);
     commands.spawn(camera_bundle);
@@ -110,41 +102,15 @@ fn setup(mut commands: Commands) {
             }),
         )
         .insert(FpsText);
+
+    let field = Field::new(0, BLOCK_SIZE);
+    Field::spawn(&mut commands, field, true, Vec3::new(-500., 0., 0.));
+
+    app_state.set(AppState::Playing);
 }
 
 fn setup_game(mut spawn_mino_events: EventWriter<SpawnMinoEvent>) {
     spawn_mino_events.send(SpawnMinoEvent);
-}
-
-fn wait_for_players(
-    mut commands: Commands,
-    mut client: ResMut<RenetClient>,
-    local_player_id: Res<LocalPlayerId>,
-    mut game_state: ResMut<NextState<GameState>>,
-) {
-    while let Some(message) = client.receive_message(DefaultChannel::ReliableOrdered) {
-        match bincode::deserialize(&message).unwrap() {
-            ServerMessage::PlayerConnected { id } => {
-                info!("Player connected: {}", id);
-
-                // TODO: 相手のフィールドを並べて表示する
-                if id == local_player_id.0 {
-                    let filed = Field::new(id, BLOCK_SIZE);
-                    Field::spawn(&mut commands, filed, true, Vec3::new(-500., 0., 0.));
-                } else {
-                    let filed = Field::new(id, BLOCK_SIZE);
-                    Field::spawn(&mut commands, filed, false, Vec3::new(500., 0., 0.));
-                }
-            }
-            ServerMessage::PlayerDisconnected { id: _id } => {
-                // TODO: remove field
-            }
-            ServerMessage::GameStart => {
-                info!("Game start");
-                game_state.set(GameState::Playing);
-            }
-        }
-    }
 }
 
 fn fps_system(diagnostic: Res<DiagnosticsStore>, mut query: Query<&mut Text, With<FpsText>>) {
