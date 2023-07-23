a48f4ad6999963761ea48acedd97eb553dd932b2
Shuntaro Nishizawa <me@s2n.tech>
2023-07-23 18:09:50 +0900

    fix: Fix i mino SRS

--- client/src/movement.rs
+++ client/src/movement.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 use crate::{
     block::Block,
     field::{Field, LocalField, FIELD_WIDTH},
-    mino::{Angle, Mino, MinoPosition},
+    mino::{shape::MinoShape, Angle, Mino, MinoPosition},
     position::Position,
     timer::{DROP_INTERVAL, SOFT_DROP_INTERVAL},
 };
@@ -84,7 +84,7 @@ pub fn handle_move_event(
                     .collect::<Vec<_>>();
 
                 let new_angle = get_new_angle(mino.angle, *direction);
-                let deltas = get_srs_deltas(mino.angle, new_angle);
+                let deltas = get_srs_deltas(mino.angle, new_angle, mino.shape);
 
                 if let Some(delta) = deltas.iter().find_map(|delta| {
                     if !is_collision(
@@ -141,19 +141,33 @@ fn get_new_angle(angle: Angle, direction: Direction) -> Angle {
     }
 }
 
-fn get_srs_deltas(angle: Angle, new_angle: Angle) -> &'static [Position] {
+fn get_srs_deltas(angle: Angle, new_angle: Angle, shape: MinoShape) -> &'static [Position] {
     use Angle::*;
 
-    match (angle, new_angle) {
-        (Deg0, Deg90) => SRS_DELTAS_0_TO_90,
-        (Deg90, Deg0) => SRS_DELTAS_90_TO_0,
-        (Deg90, Deg180) => SRS_DELTAS_90_TO_180,
-        (Deg180, Deg90) => SRS_DELTAS_180_TO_90,
-        (Deg180, Deg270) => SRS_DELTAS_180_TO_270,
-        (Deg270, Deg180) => SRS_DELTAS_270_TO_180,
-        (Deg270, Deg0) => SRS_DELTAS_270_TO_0,
-        (Deg0, Deg270) => SRS_DELTAS_0_TO_270,
-        (_, _) => unreachable!(),
+    if shape != MinoShape::I {
+        match (angle, new_angle) {
+            (Deg0, Deg90) => SRS_DELTAS_0_TO_90,
+            (Deg90, Deg0) => SRS_DELTAS_90_TO_0,
+            (Deg90, Deg180) => SRS_DELTAS_90_TO_180,
+            (Deg180, Deg90) => SRS_DELTAS_180_TO_90,
+            (Deg180, Deg270) => SRS_DELTAS_180_TO_270,
+            (Deg270, Deg180) => SRS_DELTAS_270_TO_180,
+            (Deg270, Deg0) => SRS_DELTAS_270_TO_0,
+            (Deg0, Deg270) => SRS_DELTAS_0_TO_270,
+            (_, _) => unreachable!(),
+        }
+    } else {
+        match (angle, new_angle) {
+            (Deg0, Deg90) => SRS_DELTAS_0_TO_90_I_MINO,
+            (Deg90, Deg0) => SRS_DELTAS_90_TO_0_I_MINO,
+            (Deg90, Deg180) => SRS_DELTAS_90_TO_180_I_MINO,
+            (Deg180, Deg90) => SRS_DELTAS_180_TO_90_I_MINO,
+            (Deg180, Deg270) => SRS_DELTAS_180_TO_270_I_MINO,
+            (Deg270, Deg180) => SRS_DELTAS_270_TO_180_I_MINO,
+            (Deg270, Deg0) => SRS_DELTAS_270_TO_0_I_MINO,
+            (Deg0, Deg270) => SRS_DELTAS_0_TO_270_I_MINO,
+            (_, _) => unreachable!(),
+        }
     }
 }
 
@@ -235,4 +249,61 @@ static SRS_DELTAS_0_TO_270: &[Position] = &[
     Position::new(1, 1),
     Position::new(0, -2),
     Position::new(1, -2),
+];
+
+static SRS_DELTAS_0_TO_90_I_MINO: &[Position] = &[
+    Position::new(0, 0),
+    Position::new(-2, 0),
+    Position::new(1, 0),
+    Position::new(-2, -1),
+    Position::new(1, 2),
+];
+static SRS_DELTAS_90_TO_0_I_MINO: &[Position] = &[
+    Position::new(0, 0),
+    Position::new(2, 0),
+    Position::new(-1, 0),
+    Position::new(2, 1),
+    Position::new(-1, -2),
+];
+static SRS_DELTAS_90_TO_180_I_MINO: &[Position] = &[
+    Position::new(0, 0),
+    Position::new(-1, 0),
+    Position::new(2, 0),
+    Position::new(-1, 2),
+    Position::new(2, -1),
+];
+static SRS_DELTAS_180_TO_90_I_MINO: &[Position] = &[
+    Position::new(0, 0),
+    Position::new(1, 0),
+    Position::new(-2, 0),
+    Position::new(1, -2),
+    Position::new(-2, 1),
+];
+static SRS_DELTAS_180_TO_270_I_MINO: &[Position] = &[
+    Position::new(0, 0),
+    Position::new(2, 0),
+    Position::new(-1, 0),
+    Position::new(2, 1),
+    Position::new(-1, -2),
+];
+static SRS_DELTAS_270_TO_180_I_MINO: &[Position] = &[
+    Position::new(0, 0),
+    Position::new(-2, 0),
+    Position::new(1, 0),
+    Position::new(-2, -1),
+    Position::new(1, 2),
+];
+static SRS_DELTAS_270_TO_0_I_MINO: &[Position] = &[
+    Position::new(0, 0),
+    Position::new(1, 0),
+    Position::new(-2, 0),
+    Position::new(1, -2),
+    Position::new(-2, 1),
+];
+static SRS_DELTAS_0_TO_270_I_MINO: &[Position] = &[
+    Position::new(0, 0),
+    Position::new(-1, 0),
+    Position::new(2, 0),
+    Position::new(-1, 2),
+    Position::new(2, -1),
 ];
